2299fe0e12b0bcf9abd3f9dc497fdb726043a982
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-04-01 02:06:09 +0300

    Added first setting input

--- app/src/main/java/com/yuriylisovskiy/er/SettingsActivity.java
+++ app/src/main/java/com/yuriylisovskiy/er/SettingsActivity.java
@@ -1,11 +1,17 @@
 package com.yuriylisovskiy.er;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
+import com.yuriylisovskiy.er.settings.Defaults;
 import com.yuriylisovskiy.er.settings.Prefs;
 import com.yuriylisovskiy.er.settings.Theme;
 
@@ -35,6 +41,42 @@ public class SettingsActivity extends AppCompatActivity {
 			actionBar.setDisplayHomeAsUpEnabled(true);
 			actionBar.setDisplayShowHomeEnabled(true);
 		}
+
+		RadioGroup fontSizeSetting = findViewById(R.id.font_settings);
+		if (fontSizeSetting != null) {
+			switch (prefs.fontSize()) {
+				case Defaults.FONT_SMALL:
+					fontSizeSetting.check(R.id.font_small);
+					break;
+				default:
+				case Defaults.FONT_MEDIUM:
+					fontSizeSetting.check(R.id.font_medium);
+					break;
+				case Defaults.FONT_LARGE:
+					fontSizeSetting.check(R.id.font_large);
+					break;
+			}
+			fontSizeSetting.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(RadioGroup group, int checkedId) {
+					switch (checkedId) {
+						case R.id.font_small:
+							prefs.setFontSize(Defaults.FONT_SMALL);
+							break;
+						default:
+						case R.id.font_medium:
+							prefs.setFontSize(Defaults.FONT_MEDIUM);
+							break;
+						case R.id.font_large:
+							prefs.setFontSize(Defaults.FONT_LARGE);
+							break;
+					}
+					Toast.makeText(SettingsActivity.this, Integer.toString(prefs.fontSize()), Toast.LENGTH_LONG).show();
+				}
+			});
+		}
+
+		findViewById(R.id.settings_progress_bar).setVisibility(View.GONE);
 	}
 
 	@Override
--- app/src/main/java/com/yuriylisovskiy/er/settings/Defaults.java
+++ app/src/main/java/com/yuriylisovskiy/er/settings/Defaults.java
@@ -1,22 +1,22 @@
 package com.yuriylisovskiy.er.settings;
 
-class Defaults {
+public class Defaults {
 
-	final static int FONT_SMALL = 14;
-	final static int FONT_MEDIUM = 18;
-	final static int FONT_LARGE = 22;
+	final public static int FONT_SMALL = 14;
+	final public static int FONT_MEDIUM = 16;
+	final public static int FONT_LARGE = 22;
 
-	final static String UK_UA = "uk_UA";
-	final static String EN_US = "en_US";
+	final public static String UK_UA = "uk_UA";
+	final public static String EN_US = "en_US";
 
-	final static boolean IS_DARK_THEME = false;
-	final static int FONT_SIZE = FONT_SMALL;
-	final static String LANG = EN_US;
-	final static int MAX_BACKUPS = 5;
-	final static boolean REMOVE_EVENT_AFTER_TIME_UP = true;
-	final static boolean RUN_WITH_SYSTEM_START = true;
+	final public static boolean IS_DARK_THEME = false;
+	final public static int FONT_SIZE = FONT_MEDIUM;
+	final public static String LANG = EN_US;
+	final public static int MAX_BACKUPS = 5;
+	final public static boolean REMOVE_EVENT_AFTER_TIME_UP = true;
+	final public static boolean RUN_WITH_SYSTEM_START = true;
 
 	// In minutes
-	final static int REMIND_TIME = 1;
-	final static boolean INCLUDE_SETTINGS_BACKUP = true;
+	final public static int REMIND_TIME = 1;
+	final public static boolean INCLUDE_SETTINGS_BACKUP = true;
 }
