a2c9a4e6158a49a78d756a839515dc3e50bcfe33
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2019-04-25 20:20:29 +0300

    Added title, description, date and time inputs for event

--- app/src/main/java/com/yuriylisovskiy/er/EventActivity.java
+++ app/src/main/java/com/yuriylisovskiy/er/EventActivity.java
@@ -1,6 +1,22 @@
 package com.yuriylisovskiy.er;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
 public class EventActivity extends BaseActivity {
+
+	private Calendar _dateAndTime = Calendar.getInstance();
+	private TextView _eventTimeLabel;
+	private TextView _eventDateLabel;
+	TimePickerDialog.OnTimeSetListener timeSetListener;
+	DatePickerDialog.OnDateSetListener dateSetListener;
 
 	@Override
 	protected void initLayouts() {
@@ -10,7 +26,60 @@ public class EventActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate() {
+		this.timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+			public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+				_dateAndTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+				_dateAndTime.set(Calendar.MINUTE, minute);
+				setInitialDateTime();
+			}
+		};
 
+		this.dateSetListener = new DatePickerDialog.OnDateSetListener() {
+			public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+				_dateAndTime.set(Calendar.YEAR, year);
+				_dateAndTime.set(Calendar.MONTH, monthOfYear);
+				_dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+				setInitialDateTime();
+			}
+		};
+
+		this._eventDateLabel = findViewById(R.id.event_date);
+		this._eventTimeLabel = findViewById(R.id.event_time);
+		setInitialDateTime();
+	}
+
+	private void setInitialDateTime() {
+
+		long timeInMillis = this._dateAndTime.getTimeInMillis();
+		this._eventDateLabel.setText(
+			DateUtils.formatDateTime(
+				this,
+				timeInMillis,
+				DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+			)
+		);
+		this._eventTimeLabel.setText(
+			DateUtils.formatDateTime(
+				this,
+				timeInMillis,
+				DateUtils.FORMAT_SHOW_TIME
+			)
+		);
+	}
+
+	public void setDate(View v) {
+		new DatePickerDialog(EventActivity.this, this.dateSetListener,
+				_dateAndTime.get(Calendar.YEAR),
+				_dateAndTime.get(Calendar.MONTH),
+				_dateAndTime.get(Calendar.DAY_OF_MONTH))
+				.show();
+	}
+
+	public void setTime(View v) {
+		new TimePickerDialog(EventActivity.this, this.timeSetListener,
+				_dateAndTime.get(Calendar.HOUR_OF_DAY),
+				_dateAndTime.get(Calendar.MINUTE), true)
+				.show();
 	}
 
 }
